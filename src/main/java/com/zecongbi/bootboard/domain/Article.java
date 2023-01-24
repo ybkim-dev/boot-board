8b53a4358427db41d24848bca9a499a88db98461
ybkim-dev <beanybigggy@gmail.com>
2023-01-24 12:19:58 +0900

    feat : #8 - Domain 클래스 생성
    - Article, ArticleComment Domain 클래스를 생성함

---
+++ src/main/java/com/zecongbi/bootboard/domain/Article.java
@@ -0,0 +1,15 @@
+package com.zecongbi.bootboard.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+  private Long id;
+  private String title;
+  private String content;
+  private String hashtag;
+
+  private LocalDateTime createdAt;
+  private String createdBy;
+  private LocalDateTime modifiedAt;
+  private String modifiedBy;
+}
